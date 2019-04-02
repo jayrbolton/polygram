1db7866477fb3cca18b3599f72a303a133dad308
Jay R Bolton <jayrbolton@gmail.com>
2019-04-02 08:47:06 -0700

    Build for gh-pages

--- docs/bundle.js
+++ docs/bundle.js
@@ -883,6 +883,64 @@ function Component (obj) {
 }
 
 },{"snabbdom":10,"snabbdom/h":1,"snabbdom/modules/attributes":4,"snabbdom/modules/class":5,"snabbdom/modules/dataset":6,"snabbdom/modules/eventlisteners":7,"snabbdom/modules/props":8,"snabbdom/modules/style":9}],14:[function(require,module,exports){
+// simple modal component
+const { Component, h } = require('uzu')
+
+const button = require('./button')
+
+module.exports = { Modal }
+
+function Modal () {
+  return Component({
+    isOpen: false,
+    open () {
+      this.isOpen = true
+      this._render()
+    },
+    close () {
+      this.isOpen = false
+      this._render()
+    },
+    view ({ title, content }) {
+      return h('div.code', [
+        // Backdrop
+        h('div.fixed.w-100.h-100.top-0.left-0.o-20.bg-black.z-1', {
+          class: {
+            dn: !this.isOpen
+          },
+          style: {
+          },
+          on: {
+            click: () => this.close()
+          }
+        }),
+        // Modal
+        h('div.fixed.top-1.shadow-3.bg-white.z-2', {
+          style: {
+            left: '50%',
+            width: '32rem',
+            marginLeft: '-16rem'
+          },
+          class: {
+            dn: !this.isOpen
+          }
+        }, [
+          h('div.flex.justify-between.bb.b--black-20.pa3', [
+            h('h1.f4.ma0', [
+              title
+            ]),
+            button('Close', () => this.close())
+          ]),
+          h('div.pa3', [
+            content
+          ])
+        ])
+      ])
+    }
+  })
+}
+
+},{"./button":16,"uzu":13}],15:[function(require,module,exports){
 const { Component, h } = require('uzu')
 
 const field = require('./field')
@@ -897,8 +955,20 @@ const start = window.performance.now()
 function Rectangle (canvasState) {
   return Component({
     name: 'rect-' + id++,
-    props: {
+    flags: {
+      hasFill: true,
+      hasRotation: false,
+      hasStroke: false
+    },
+    utils: {
+      sin: Math.sin,
+      pi: Math.PI,
+      mouseX: () => document._mouseX,
+      mouseY: () => document._mouseY,
       ts: () => window.performance.now() - start,
+      rand: (max) => Math.floor(Math.random() * Math.floor(max))
+    },
+    props: {
       copies: 1,
       x: 50,
       y: 50,
@@ -912,7 +982,7 @@ function Rectangle (canvasState) {
       strokeGreen: 0,
       strokeBlue: 0,
       strokeAlpha: 1,
-      strokeWidth: 0,
+      strokeWidth: 2,
       radians: 0,
       rotateX: 0,
       rotateY: 0,
@@ -931,25 +1001,28 @@ function Rectangle (canvasState) {
     },
     drawOne (ctx, i) {
       let props = {}
+      const defs = Object.assign(this.utils, this.props)
       for (let name in this.props) {
-        props[name] = evaluate(this.props[name], this.props)
+        props[name] = evaluate(this.props[name], defs)
       }
       this.props.i = i
-      const strokeWidth = props.strokeWidth
       const x = props.x
       const y = props.y
       ctx.save()
-      if (props.radians) {
+      // TODO: if (this.flags.hasRotation)
+      if (this.flags.hasRotation) {
         const rotx = x + props.rotateX
         const roty = y + props.rotateY
         ctx.translate(rotx, roty)
         ctx.rotate(props.radians)
         ctx.translate(-rotx, -roty)
       }
-      ctx.scale(props.scaleX, props.scaleY)
-      ctx.fillStyle = 'rgba(' + props.fillRed + ', ' + props.fillGreen + ', ' + props.fillBlue + ', ' + props.fillAlpha + ')'
-      ctx.fillRect(x, y, props.width, props.height)
-      if (strokeWidth) {
+      if (this.flags.hasFill) {
+        ctx.fillStyle = 'rgba(' + props.fillRed + ', ' + props.fillGreen + ', ' + props.fillBlue + ', ' + props.fillAlpha + ')'
+        ctx.fillRect(x, y, props.width, props.height)
+      }
+      if (this.flags.hasStroke) {
+        const strokeWidth = props.strokeWidth
         ctx.strokeStyle = 'rgba(' + props.strokeRed + ', ' + props.strokeGreen + ', ' + props.strokeBlue + ', ' + props.strokeAlpha + ')'
         ctx.lineWidth = strokeWidth
         ctx.strokeRect(x, y, props.width, props.height)
@@ -965,24 +1038,79 @@ function Rectangle (canvasState) {
         field(this, 'height', ['height'], vars),
         field(this, 'x', ['x'], vars),
         field(this, 'y', ['y'], vars),
-        field(this, 'fill red', ['fillRed'], vars),
-        field(this, 'fill green', ['fillGreen'], vars),
-        field(this, 'fill blue', ['fillBlue'], vars),
-        field(this, 'fill alpha', ['fillAlpha'], vars),
-        field(this, 'stroke red', ['fillAlpha'], vars),
-        field(this, 'stroke green', ['fillAlpha'], vars),
-        field(this, 'stroke blue', ['fillAlpha'], vars),
-        field(this, 'stroke alpha', ['fillAlpha'], vars),
-        field(this, 'stroke width', ['strokeWidth'], vars),
-        field(this, 'radians', ['radians'], vars),
-        field(this, 'origin X', ['originX'], vars),
-        field(this, 'origin Y', ['originY'], vars)
+        // Fill
+        fieldGroup(this, {
+          flag: 'hasFill',
+          name: 'fill',
+          children: [
+            field(this, 'fill red', ['fillRed'], vars),
+            field(this, 'fill green', ['fillGreen'], vars),
+            field(this, 'fill blue', ['fillBlue'], vars),
+            field(this, 'fill alpha', ['fillAlpha'], vars)
+          ]
+        }),
+
+        fieldGroup(this, {
+          flag: 'hasStroke',
+          name: 'stroke',
+          children: [
+            field(this, 'stroke red', ['fillAlpha'], vars),
+            field(this, 'stroke green', ['fillAlpha'], vars),
+            field(this, 'stroke blue', ['fillAlpha'], vars),
+            field(this, 'stroke alpha', ['fillAlpha'], vars),
+            field(this, 'stroke width', ['strokeWidth'], vars)
+          ]
+        }),
+
+        fieldGroup(this, {
+          flag: 'hasRotation',
+          name: 'rotation',
+          children: [
+            field(this, 'radians', ['radians'], vars),
+            field(this, 'origin X', ['rotateX'], vars),
+            field(this, 'origin Y', ['rotateY'], vars)
+          ]
+        })
       ])
     }
   })
 }
 
-},{"../utils/evaluate":18,"./field":15,"uzu":13}],15:[function(require,module,exports){
+function fieldGroup (shape, opts) {
+  const { flag, name, children } = opts
+  const htmlID = 'field-flag-' + name
+  return h('div.bl.bw2.b--black-20.pl1.pb1.mb2', [
+    h('input', {
+      props: { type: 'checkbox', checked: shape.flags[flag], id: htmlID },
+      on: {
+        change: () => {
+          // Toggle field group flag
+          shape.flags[flag] = !shape.flags[flag]
+          shape._render()
+        }
+      }
+    }),
+    h('label.pointer.code.ml2.b.black-60', { props: { htmlFor: htmlID } }, name),
+    h('div.mt2', {
+      props: {
+        hidden: !shape.flags[flag]
+      }
+    }, children)
+  ])
+}
+
+},{"../utils/evaluate":20,"./field":17,"uzu":13}],16:[function(require,module,exports){
+const { h } = require('uzu')
+
+function button (txt, onclick) {
+  return h('button.bg-white.ba.b--black-10.f6.pointer.dib.code', {
+    on: { click: onclick }
+  }, txt)
+}
+
+module.exports = button
+
+},{"uzu":13}],17:[function(require,module,exports){
 const { h } = require('uzu')
 const fieldset = require('./fieldset')
 
@@ -999,12 +1127,12 @@ function field (elem, label, prop, vars) {
     }
   })
   return fieldset([
-    h('label.code', label),
+    h('label.code.dib.mb1', label),
     h('div', { css: { root: [ 'float: right' ] } }, inputs)
   ])
 }
 
-},{"./fieldset":16,"uzu":13}],16:[function(require,module,exports){
+},{"./fieldset":18,"uzu":13}],18:[function(require,module,exports){
 const { h } = require('uzu')
 
 module.exports = fieldset
@@ -1030,25 +1158,24 @@ function fieldset (children) {
   }, children)
 }
 
-},{"uzu":13}],17:[function(require,module,exports){
+},{"uzu":13}],19:[function(require,module,exports){
 const { Component, h } = require('uzu')
 
+const { Modal } = require('./components/Modal')
+const button = require('./components/button')
+
 // TODO
+// - ability to save work
 // - global constants
 // - cache evaluated formulas as functions
-// - grid of rectangles, evenly spaced x and y
 // - try to remove ctx.save and restore
-// - checkboxes for fill, stroke, etc
 // - general shapes
 // - canvas fill
-// - when creating an elem, repeat props from last elem
-// - ++ keyframes/timed values (start and end with an easing function)
 
 // Components
 const { Rectangle } = require('./components/Rectangle')
 
 // Utils/views
-const newElemButton = require('./utils/newElemButton')
 const fieldset = require('./components/fieldset')
 
 function App () {
@@ -1066,54 +1193,83 @@ function App () {
   })
 }
 
-const start = window.performance.now()
-
 function CanvasState () {
   return Component({
     // Dynamic variables for use in properties
-    vars: {
-      canvasWidth: () => 1000,
-      canvasHeight: () => 1000,
-      ts: () => window.performance.now() - start,
-      pi: () => Math.PI,
-      mouseX: () => document._mouseX,
-      mouseY: () => document._mouseY,
-      scrollTop: () => document.scrollTop,
-      windowHeight: () => window.innerHeight,
-      windowWidth: () => window.innerWidth
-    },
-    elems: { },
+    canvasWidth: 1000,
+    canvasHeight: 1000,
+    elems: {},
     elemOrder: [],
+    savedModal: Modal(),
+    openModal: Modal(),
     view () {
       const elems = this.elemOrder.map(elem => {
         return h('div', { key: elem.name }, [
-          h('div.b.pointer.bb.b--black-20.mv1.code.pv1.flex.justify-between', {
-            on: { click: () => { elem.toggleFormOpen() } }
-          }, [
-            elem.name,
-            removeButton(this, elem)
+          h('div.b.bb.b--black-20.mv1.code.pv1.flex.justify-between', [
+            h('span.pointer.dib', {
+              on: { click: () => elem.toggleFormOpen() }
+            }, elem.name),
+            h('div', [
+              copyButton(this, elem),
+              removeButton(this, elem)
+            ])
           ]),
           elem.view()
         ])
       })
-      return h('div.mw5.bg-light-gray.pa2', {
-        css: {
-          root: [
-            'padding: 1rem',
-            'width: 300px',
-            'float: left',
-            'background: #f8f8f8'
-          ]
+      return h('div.bg-light-gray.pa2', {
+        style: {
+          width: '20rem'
         }
       }, [
+        this.openModal.view({
+          title: 'Open',
+          content: h('div', [
+            h('form', {
+              on: {
+                submit: ev => {
+                  ev.preventDefault()
+                  const jsonState = ev.currentTarget.querySelector('textarea').value
+                  this.openModal.close()
+                  restore(jsonState, this)
+                }
+              }
+            }, [
+              h('textarea.w-100', {
+                props: { rows: 4 }
+              }),
+              button('Load')
+            ])
+          ])
+        }),
+        this.savedModal.view({
+          title: 'Saved!',
+          content: h('div', [
+            h('p', [
+              'Drawing state:',
+              h('div', [
+                h('textarea.w-100', {
+                  props: {
+                    value: this.jsonState,
+                    rows: 4
+                  }
+                })
+              ])
+            ])
+          ])
+        }),
+        h('div.flex.justify-end', [
+          saveButton(this),
+          openButton(this)
+        ]),
         fieldset([
           h('label.code', { css: { root: ['font-family: mono'] } }, 'canvas-width'),
-          h('input.code.f6.pa1', {
-            props: { type: 'number', value: this.vars.canvasWidth() },
+          h('input.code.f6.pa1.w-100', {
+            props: { type: 'number', value: this.canvasWidth },
             on: {
               input: ev => {
                 const val = ev.currentTarget.value
-                this.vars.canvasWidth = () => val
+                this.canvasWidth = val
                 document._canvas.width = val
               }
             }
@@ -1121,12 +1277,12 @@ function CanvasState () {
         ]),
         fieldset([
           h('label.code', { css: { root: ['font-family: mono'] } }, 'canvas-height'),
-          h('input.code.f6.pa1', {
-            props: { type: 'number', value: this.vars.canvasHeight() },
+          h('input.code.f6.pa1.w-100', {
+            props: { type: 'number', value: this.canvasHeight },
             on: {
               input: ev => {
                 const val = ev.currentTarget.value
-                this.vars.canvasHeight = () => val
+                this.canvasHeight = val
                 document._canvas.height = val
               }
             }
@@ -1134,7 +1290,7 @@ function CanvasState () {
         ]),
         h('div', [
           // newElemButton(this, Value, 'value'),
-          newElemButton(this, Rectangle, 'rectangle')
+          newElemButton(this, Rectangle, 'shape')
         ]),
         h('div', elems)
       ])
@@ -1169,7 +1325,7 @@ function Canvas (canvasState) {
     view () {
       return h('canvas.fixed.top-0', {
         style: {
-          left: '16rem'
+          left: '20rem'
         },
         props: {
           id: 'tutorial'
@@ -1177,8 +1333,8 @@ function Canvas (canvasState) {
         hook: {
           insert: (vnode) => {
             const elm = vnode.elm
-            elm.width = canvasState.vars.canvasWidth()
-            elm.height = canvasState.vars.canvasHeight()
+            elm.width = canvasState.canvasWidth
+            elm.height = canvasState.canvasHeight
             document._canvas = elm
             const ctx = elm.getContext('2d')
             ctx.globalCompositeOperation = 'destination-over'
@@ -1186,7 +1342,7 @@ function Canvas (canvasState) {
             ctx.save()
             function draw (ts) {
               document._ts = ts
-              ctx.clearRect(0, 0, canvasState.vars.canvasWidth(), canvasState.vars.canvasHeight())
+              ctx.clearRect(0, 0, canvasState.canvasWidth, canvasState.canvasHeight)
               for (let name in canvasState.elems) {
                 let shape = canvasState.elems[name]
                 if (shape.draw) shape.draw(ctx)
@@ -1201,16 +1357,88 @@ function Canvas (canvasState) {
   })
 }
 
-function removeButton (app, elem) {
-  return h('button.bg-white.ba.b--black-10.f6', {
-    on: {
-      click: () => {
-        delete app.elems[elem.name]
-        app.elemOrder = app.elemOrder.filter(e => e.name !== elem.name)
-        app._render()
-      }
+// Takes the full app component, plus a single element (like a Rectangle)
+function removeButton (canvasState, elem) {
+  return button('Remove', () => {
+    delete canvasState.elems[elem.name]
+    canvasState.elemOrder = canvasState.elemOrder.filter(e => e.name !== elem.name)
+    canvasState._render()
+  })
+}
+
+// Takes the full app component, plus a single element (like a Rectangle)
+function copyButton (canvasState, elem) {
+  return button('Copy', () => {
+    const newElem = Rectangle(canvasState)
+    const props = Object.create(elem.props)
+    const flags = Object.create(elem.flags)
+    newElem.props = props
+    newElem.flags = flags
+    canvasState.elems[newElem.name] = newElem
+    canvasState.elemOrder.push(newElem)
+    canvasState._render()
+  })
+}
+
+// Create a new shape
+function newElemButton (canvasState, constructor, name) {
+  return button('Add ' + name, () => {
+    const cmp = constructor(canvasState)
+    canvasState.elems[cmp.name] = cmp
+    canvasState.elemOrder.push(cmp)
+    canvasState._render()
+  })
+}
+
+// Save the state of the drawing
+function saveButton (canvasState) {
+  return button('Save', () => {
+    canvasState.jsonState = persist(canvasState)
+    canvasState.savedModal.open()
+    canvasState._render()
+  })
+}
+
+// Open a new drawing
+function openButton (canvasState) {
+  return button('Open', () => {
+    canvasState.openModal.open()
+  })
+}
+
+function persist (canvasState) {
+  const getElem = elem => {
+    return {
+      flags: elem.flags,
+      props: elem.props,
+      name: elem.name
     }
-  }, ['Remove'])
+  }
+  const elemOrder = canvasState.elemOrder.map(getElem)
+  const json = JSON.stringify({
+    canvasWidth: canvasState.canvasWidth,
+    canvasHeight: canvasState.canvasHeight,
+    elemOrder
+  })
+  localStorage.setItem('canvas-state', json)
+  return json
+}
+
+function restore (json, canvasState) {
+  const data = JSON.parse(json)
+  canvasState.canvasWidth = data.canvasWidth
+  canvasState.canvasHeight = data.canvasHeight
+  canvasState.elems = {}
+  canvasState.elemOrder = []
+  data.elemOrder.forEach(elemData => {
+    const elem = Rectangle(canvasState)
+    elem.props = elemData.props
+    elem.flags = elemData.flags
+    elem.name = elemData.name
+    canvasState.elems[elem.name] = elem
+    canvasState.elemOrder.push(elem)
+  })
+  canvasState._render()
 }
 
 // Get the mouse x/y coords globally
@@ -1219,9 +1447,21 @@ document.body.addEventListener('mousemove', ev => {
   document._mouseY = ev.clientY
 })
 
-document.body.appendChild(App().view().elm)
+const app = App()
 
-},{"./components/Rectangle":14,"./components/fieldset":16,"./utils/newElemButton":19,"uzu":13}],18:[function(require,module,exports){
+const initialState = localStorage.getItem('canvas-state')
+if (initialState) {
+  try {
+    restore(initialState, app.canvasState)
+  } catch (e) {
+    console.error('Unable to restore localstorage state:', e)
+    localStorage.removeItem('canvas-state')
+  }
+}
+
+document.body.appendChild(app.view().elm)
+
+},{"./components/Modal":14,"./components/Rectangle":15,"./components/button":16,"./components/fieldset":18,"uzu":13}],20:[function(require,module,exports){
 module.exports = evaluate
 
 // We use `with` and `eval` because it's easy and simple and the security 
@@ -1240,22 +1480,4 @@ function evaluate (str, vars) {
 
 window.evaluate = evaluate
 
-},{}],19:[function(require,module,exports){
-const { h } = require('uzu')
-
-module.exports = newElemButton
-
-function newElemButton (parentCmp, constructor, name) {
-  return h('button.bg-white.ba.code.pa2.pointer.mb3', {
-    on: {
-      click: () => {
-        const cmp = constructor(parentCmp)
-        parentCmp.elems[cmp.name] = cmp
-        parentCmp.elemOrder.push(cmp)
-        parentCmp._render()
-      }
-    }
-  }, ['Add ', name])
-}
-
-},{"uzu":13}]},{},[17]);
+},{}]},{},[19]);
